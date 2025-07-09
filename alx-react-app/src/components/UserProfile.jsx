2879e18357867492ab5d349b8e6c80c202bd32ad
arefatmoh <arefat.mohammed-ug@aau.edu.et>
2025-07-09 12:24:26 +0300

    Fix: Add 'Bio:'

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -11,7 +11,7 @@ const UserProfile = (props) => {
     }}>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p> {/* <-- Capital 'B' added here */}
     </div>
   );
 };
