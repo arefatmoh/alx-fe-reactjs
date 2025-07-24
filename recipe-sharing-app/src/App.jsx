dfce824e9cd3095ae09602aa9941c57c54f62399
arefatmoh <arefat.mohammed-ug@aau.edu.et>
2025-07-24 16:17:45 +0300

    Add search and filtering functionality with Zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
-    <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
